399270bfa9a93aefb2af988aaa863000ead09543
dekelgustavo <dekelgustavo@gmail.com>
2022-09-07 19:36:40 -0300

    feat: implements userSchema

---
+++ src/modules/auth/schemas/user.schema.ts
@@ -0,0 +1,26 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+export enum UserRoles {
+  ADMIN = 'admin',
+  USER = 'user',
+}
+
+@Schema()
+export class User extends Document {
+  @Prop()
+  name: string;
+
+  @Prop({ unique: [true, 'Duplicated email entered'] })
+  email: string;
+
+  @Prop({ select: false })
+  password: string;
+
+  @Prop({
+    enum: UserRoles,
+    default: UserRoles.USER,
+  })
+  role: UserRoles;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
